5990f0c61a8f50048ad68b4dc8e1f92d0ddecc38
Amy Shackles <amyshackles@gmail.com>
2023-09-17 17:31:44 -0500

    Fix handleRange, improve testing

--- __tests__/handleRange.js
+++ __tests__/handleRange.js
@@ -1,16 +1,92 @@
 const { handleRange } = require("../src/handleRange");
 
 describe("handleRange", () => {
+    const lastElementUnquantified = {
+        quantifier: "exactlyOne",
+        regex: "i",
+        type: "literal",
+        value: "i",
+    };
     test("should handle minimum quantifier", () => {
-        const lastElement = {
-            quantifier: "exactlyOne",
-            regex: "i",
-            type: "literal",
-            value: "i"
-        };
+        const lastElement = { ...lastElementUnquantified };
+        const betweenBraces = "3,";
+        const index = 5;
+        const result = handleRange(lastElement, betweenBraces, index);
+        expect(lastElement.quantifier).toEqual("atLeast3");
+        expect(lastElement.regex).toEqual(`${lastElementUnquantified.regex}{${betweenBraces}}`);
+        expect(lastElement.value).toEqual(`"${lastElementUnquantified.value}" repeated at least 3 times`);
+        expect(result).toEqual({index: index + betweenBraces.length + 1});
+    });
+    test("should handle if no minimum quantifier given", () => {
+        const lastElement = { ...lastElementUnquantified };
+        const betweenBraces = ",5";
+        const index = 2;
+        const result = handleRange(lastElement, betweenBraces, index);
+        expect(result).toEqual({index: index + 1, token: { quantifier: "exactlyOne", regex: "{", type: "literal", value: "{"}});
+        expect(lastElement.quantifier).toEqual("exactlyOne");
+    });
+    test("should handle if minimum quantifier is not numeric", () => {
+        const lastElement = { ...lastElementUnquantified };
+        const betweenBraces = "a,5";
+        const index = 2;
+        const result = handleRange(lastElement, betweenBraces, index);
+        expect(result).toEqual({index: index + 1, token: { quantifier: "exactlyOne", regex: "{", type: "literal", value: "{"}});
+        expect(lastElement.quantifier).toEqual("exactlyOne");
+    });
+    test("should handle minimum and maximum quantifiers", () => {
+        const lastElement = { ...lastElementUnquantified };
+        const betweenBraces = "3,5";
+        const index = 2;
+        const result = handleRange(lastElement, betweenBraces, index);
+        expect(lastElement.quantifier).toEqual("3to5");
+        expect(lastElement.regex).toEqual(`${lastElementUnquantified.regex}{${betweenBraces}}`);
+        expect(lastElement.value).toEqual(`"${lastElementUnquantified.value}" repeated at least 3 times and no more than 5 times`);
+        expect(result).toEqual({index: index + betweenBraces.length + 1});
+    });
+    test("should handle if minimum and maximum quantifiers are the same", () => {
+        const lastElement = { ...lastElementUnquantified };
+        const betweenBraces = "3,3";
+        const index = 2;
+        const result = handleRange(lastElement, betweenBraces, index);
+        expect(lastElement.quantifier).toEqual("exactly3");
+        expect(lastElement.regex).toEqual(`${lastElementUnquantified.regex}{${betweenBraces}}`);
+        expect(lastElement.value).toEqual(`"${lastElementUnquantified.value.repeat(3)}"`);
+        expect(result).toEqual({index: index + betweenBraces.length + 1});
+    });
+    test("should handle if only one quantifier given", () => {
+        const lastElement = { ...lastElementUnquantified };
+        const betweenBraces = "7";
+        const index = 2;
+        const result = handleRange(lastElement, betweenBraces, index);
+        expect(lastElement.quantifier).toEqual("exactly7");
+        expect(lastElement.regex).toEqual(`${lastElementUnquantified.regex}{${betweenBraces}}`);
+        expect(lastElement.value).toEqual(`"${lastElementUnquantified.value.repeat(7)}"`);
+        expect(result).toEqual({index: index + betweenBraces.length + 1});
+    });
+    test("should handle if maximum quantifier is not numeric", () => {
+        const lastElement = { ...lastElementUnquantified };
+        const betweenBraces = "3,a";
+        const index = 2;
+        const result = handleRange(lastElement, betweenBraces, index);
+        expect(result).toEqual({index: index + 1, token: { quantifier: "exactlyOne", regex: "{", type: "literal", value: "{"}});
+        expect(lastElement.quantifier).toEqual("exactlyOne");
+    });
+    test("should throw if previous element is quantified", () => {
+        const lastElement = { ...lastElementUnquantified, quantifier: "zeroOrMore"};
         const betweenBraces = "3,";
         const index = 2;
-        handleRange(lastElement, betweenBraces, index);
-        expect(lastElement.quantifier).toEqual("atLeast3");
+        expect(() => {
+            handleRange(lastElement, betweenBraces, index);
+        }).toThrowError("Range must follow an unquantified element");
+        expect(lastElement.quantifier).toEqual("zeroOrMore");
+    });
+    test("should throw if minimum quantifier is larger than maximum", () => {
+        const lastElement = { ...lastElementUnquantified };
+        const betweenBraces = "3,1";
+        const index = 2;
+        expect(() => {
+            handleRange(lastElement, betweenBraces, index);
+        }).toThrowError("Quantifier minimum cannot be greater than maximum");
+        expect(lastElement.quantifier).toEqual("exactlyOne");
     });
 });
--- src/handleRange.js
+++ src/handleRange.js
@@ -2,13 +2,13 @@ function getMinMax(string) {
     const [min, max] = string.split(",");
     return { max, min };
 }
-const isNumeric = (num) => (typeof(num) === "number" || typeof(num) === "string" && num.trim() !== "") && !isNaN(num);
+const isNumeric = (num) => (typeof num === "number"
+        || (typeof num === "string" && num.trim() !== ""))
+    && !isNaN(num);
 
 const handleRange = (lastElement, betweenBraces, index) => {
     if (!lastElement || lastElement.quantifier !== "exactlyOne") {
-        throw new Error(
-            "Range must follow an unquantified element"
-        );
+        throw new Error("Range must follow an unquantified element");
     }
     const { min, max } = getMinMax(betweenBraces);
     if (!isNumeric(min)) {
@@ -18,25 +18,47 @@ const handleRange = (lastElement, betweenBraces, index) => {
                 quantifier: "exactlyOne",
                 regex: "{",
                 type: "literal",
-                value: "{"
-            }
+                value: "{",
+            },
         };
-    } else if (isNumeric(min) && isNumeric(max)) {
+    }
+    if (isNumeric(max)) {
+        if (min > max)
+            throw new Error(
+                "Quantifier minimum cannot be greater than maximum",
+            );
+        if (min === max) {
+            lastElement.quantifier = `exactly${min}`;
+            lastElement.regex += `{${betweenBraces}}`;
+            lastElement.value = `"${lastElement.value.repeat(min)}"`;
+            return { index: index + betweenBraces.length + 1 };
+        }
         lastElement.quantifier = `${min}to${max}`;
         lastElement.regex += `{${betweenBraces}}`;
         lastElement.value = `"${lastElement.value}" repeated at least ${min} times and no more than ${max} times`;
-        return {index: index + betweenBraces.length + 1, };
-    } else if (isNumeric(min) && max === "") {
+        return { index: index + betweenBraces.length + 1 };
+    }
+    if (max === "") {
         lastElement.quantifier = `atLeast${min}`;
         lastElement.regex += `{${betweenBraces}}`;
         lastElement.value = `"${lastElement.value}" repeated at least ${min} times`;
-        return {index: index + betweenBraces.length + 1, };
-    } else if (isNumeric(min) && max === undefined) {
+        return { index: index + betweenBraces.length + 1 };
+    }
+    if (max === undefined) {
         lastElement.quantifier = `exactly${min}`;
         lastElement.regex += `{${betweenBraces}}`;
         lastElement.value = `"${lastElement.value.repeat(min)}"`;
-        return {index: index + betweenBraces.length + 1, };
+        return { index: index + betweenBraces.length + 1 };
     }
+    return {
+        index: index + 1,
+        token: {
+            quantifier: "exactlyOne",
+            regex: "{",
+            type: "literal",
+            value: "{",
+        }
+    };
 };
 
-module.exports = {handleRange};
+module.exports = { handleRange };
