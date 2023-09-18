ccee0baace1e8f269e2b845fad2325a1fe0b3cc8
Amy Shackles <amyshackles@gmail.com>
2023-09-17 20:47:51 -0500

    Improve getUnicode, add tests

--- __tests__/getUnicode.js
+++ __tests__/getUnicode.js
@@ -3,7 +3,7 @@ const { getUnicode } = require("../src/getUnicode.js");
 const unicodeChar = "Ӹ";
 
 describe("getUnicode", () => {
-    describe("unicodeFlagSet", () => {
+    describe("unicode flag set", () => {
         test("should return the correct token if extended unicode passed in", () => {
             expect(getUnicode("{12352}", 0, true)).toEqual({
                 nextIndex: 7,
@@ -23,6 +23,36 @@ describe("getUnicode", () => {
                     value: unicodeChar,
                 }
             });
-        }); 
+        });
+        test("should return the correct token if unicode passed in", () => {
+            expect(getUnicode("1234a", 0, true)).toEqual({
+                nextIndex: 4,
+                token: {
+                    quantifier: "exactlyOne",
+                    regex: "\\u1234",
+                    type: "unicode",
+                    value: "ሴ"
+                }
+            });
+        });
+        test("should return a literal if an invalid unicode is passed in", () => {
+            expect(getUnicode("12", 0, true)).toEqual({
+                nextIndex: 1,
+                token: {
+                    quantifier: "exactlyOne",
+                    regex: "\\u",
+                    type: "literal",
+                    value: "u",
+                }
+            }
+        );
+        });
+    });
+    describe("unicode flag not set", () => {
+        test("should throw error if extended unicode is used outside of unicode mode", () => {
+            expect(() => {
+                getUnicode("{12345}", 0, false);
+            }).toThrowError("Invalid use of extended unicode outside of unicode mode");
+        });
     });
 });
--- src/getUnicode.js
+++ src/getUnicode.js
@@ -11,23 +11,25 @@ const getUnicode = (regexString, index, unicodeFlagSet) => {
         return getUnicodeCharacter(hex, nextIndex, "unicodeExtended");
     } else if (!unicodeFlagSet && regexString[index] === "{") {
         throw new Error(
-            "Invalid use of extended unicode outside of unicode mode"
+            "Invalid use of extended unicode outside of unicode mode",
         );
     }
-    hex = regexString.slice(index, index + 5);
-    nextIndex = index + 5;
+
+    hex = regexString.slice(index, index + 4);
+    if (isNaN(parseInt(hex, 16)) || hex.length !== 4) {
+        return {
+            nextIndex: ++index,
+            token: {
+                quantifier: "exactlyOne",
+                regex: "\\u",
+                type: "literal",
+                value: "u",
+            },
+        };
+    }
+    nextIndex = index + 4;
     const unicodeCharacter = getUnicodeCharacter(hex, nextIndex, "unicode");
-    if (unicodeCharacter) 
-        return unicodeCharacter;
-    return {
-        nextIndex: ++index,
-        token: {
-            quantifier: "exactlyOne",
-            regex: "\\u",
-            type: "literal",
-            value: "u",
-        },
-    };
+    return unicodeCharacter;
 };
 
 module.exports = { getUnicode };
