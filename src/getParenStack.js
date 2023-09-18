356086a21727dda0410ab3228711dbcbd229cec5
Amy Shackles <amyshackles@gmail.com>
2023-09-17 20:10:06 -0500

    Add ignore else to getParenStack

--- src/getParenStack.js
+++ src/getParenStack.js
@@ -2,7 +2,9 @@ const getParenStack = (string) => {
     const openParen = [];
     const captures = [];
     [...string].forEach((char, index) => {
+        /* istanbul ignore else */
         if (char === "(") {
+            /* istanbul ignore else */
             if ((index > 0 && string[index - 1] !== "\\") | index == 0) {
                 openParen.push(index);
             }
